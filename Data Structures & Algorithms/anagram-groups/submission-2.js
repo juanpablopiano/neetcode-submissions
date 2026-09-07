38b51523379792840129b98a60f4b5f3788ffd14
Juan Pablo Gonzalez <59584304+juanpablopiano@users.noreply.github.com>
2026-09-07 12:30:48 -0300

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map()
+        for (let s of strs) {
+            const count = new Array(26).fill(0)
+            for (let c of s) {
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1
+            }
+            const key = count.join(',')
+            if (!map.get(key)) map.set(key, [])
+            map.get(key).push(s)
+        }
+        return [...map.values()]
+    }
+}
